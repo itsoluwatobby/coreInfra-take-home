ed95b33d92029734557ec1a8834ba95bedbb7410
LOVELY-WANDYTE\personal <itsoluwatobby@gmail.com>
2025-05-21 15:33:23 +0100

    m

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,14 +7,14 @@ const Header: React.FC = () => {
     <header className="bg-white border-b border-b-[#DEDEDF] w-full sticky top-0 z-50">
       <nav className="w-full flex items-center justify-between max-xxs:gap-5 px-3 py-2">
 
-        <div className='flex items-center cursor-default gap-3 text-xs text-[#001735]'>
+        <div className='flex items-center mr-1 cursor-default gap-3 text-xs text-[#001735]'>
           <SVGIcon type="Dashboard" className='stroke-[#001735]' />
           <span>Dashboard</span>
         </div>
 
-        <div className='flex items-center max-xxs:gap-3 max-xxs:w-full w-[19.875rem] justify-between'>
+        <div className='flex items-center max-xxs:gap-3 max-xxs:w-full w-[19.85rem] justify-between'>
 
-          <div className='rounded-2xl w-[13.4rem] border border-gray-300 bg-white flex items-center gap-2 p-2 h-8'>
+          <div className='rounded-2xl w-[13.3rem] border border-gray-300 bg-white flex items-center gap-2 p-2 h-8'>
             <SVGIcon type="Search" className='size-3 flex-none' />
             <input
               type='search'
